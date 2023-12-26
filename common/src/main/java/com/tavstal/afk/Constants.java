44ab12a84b2c9bf4d9663d27271e28aef9763966
Tavstal <tavstal@gmail.com>
2023-12-27 00:50:38 +0100

    Basic setup

---
+++ common/src/main/java/com/tavstal/afk/Constants.java
@@ -0,0 +1,11 @@
+package com.tavstal.afk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "tafk";
+	public static final String MOD_NAME = "Tavstals-AFK";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
