1ceb0fcf91bfb6826bcc4fc02d0b992de5d5c5ff
Tavstal <tavstal@gmail.com>
2024-01-03 17:26:26 +0100

    Minor changes

--- common/src/main/java/com/tavstal/afk/utils/EntityUtils.java
+++ common/src/main/java/com/tavstal/afk/utils/EntityUtils.java
@@ -2,6 +2,7 @@ package com.tavstal.afk.utils;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 
 public class EntityUtils {
 
@@ -12,5 +13,9 @@ public class EntityUtils {
    public static boolean IsPlayer(Entity entity)
    {
       return entity instanceof Player; 
+   }
+
+   public static Level GetLevel(Entity entity) {
+      return entity.level;
    }
 }
