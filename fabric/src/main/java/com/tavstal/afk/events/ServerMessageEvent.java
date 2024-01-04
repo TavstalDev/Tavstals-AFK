34b6e0965c700a425ecac167b8ef082f69221dd2
Tavstal <tavstal@gmail.com>
2024-01-04 12:38:39 +0100

    Minor changes

--- fabric/src/main/java/com/tavstal/afk/events/ServerMessageEvent.java
+++ fabric/src/main/java/com/tavstal/afk/events/ServerMessageEvent.java
@@ -2,4 +2,9 @@ package com.tavstal.afk.events;
 
 public class ServerMessageEvent {
     
+    public static final Event<ChatMessage> EVENT = EventFactory.createArrayBacked(ChatMessage.class, handlers -> (message, sender, params) -> {
+		for (ChatMessage handler : handlers) {
+			handler.onChatMessage(message, sender, params);
+		}
+	}); 
 }
